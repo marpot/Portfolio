db13fbba2e8592111c7a7f63fb8143e21dd4966d
marpot <marcin.potoczny@protonmail.com>
2022-12-18 20:10:19 +0100

    portfolio-django

--- projects/apps.py
+++ projects/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 
 class MarcinpotocznyConfig(AppConfig):
-    name = 'marcinpotoczny'
+    name = 'projects'
--- projects/views.py
+++ projects/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
 
+
 def hello_world(request):
     return render(request, 'index.html', {})
