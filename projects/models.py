c4bb44333f5482525c063ae9bb22e7a497a4acab
marpot <marcin.potoczny@protonmail.com>
2022-12-22 14:13:35 +0100

    Add models to portfolio page

--- projects/models.py
+++ projects/models.py
@@ -3,7 +3,15 @@ from django.db import models
 
 # Create your models here.
 class Project(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=25)
     description = models.TextField()
     technology = models.CharField(max_length=100)
-    image = models.FilePathField(path="/img")
+    image = models.FilePathField(path="images/")
+    github_url = models.URLField(max_length=100, null=True)
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    publish_date = models.DateTimeField()
+    content = models.TextField()
+    image = models.ImageField(upload_to='images/')
