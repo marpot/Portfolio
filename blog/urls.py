52593b0b2fb26f39f5eb9daa9bf96344dbe2b2b4
marpot <marcin.potoczny@protonmail.com>
2023-01-10 09:39:41 +0100

    Working

--- blog/urls.py
+++ blog/urls.py
@@ -7,5 +7,5 @@ from jobs.views import (home)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view (template_name="home_page/home.html"))
+    path('', TemplateView.as_view (template_name="home.html"))
 ]
